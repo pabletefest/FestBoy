cc890bf947e14eec08aba9a0e41c5a8846a23cc5
pabletefest <stemmerpablo@gmail.com>
2023-05-30 17:15:12 +0900

    Added addressing modes array to access VRAM and commented blocking VRAM and OAM code for now

--- FestBoy/src/ppu.cpp
+++ FestBoy/src/ppu.cpp
@@ -1,6 +1,12 @@
 #include "ppu.h"
 #include "gb.h"
 
+namespace gb
+{
+    // "$8000 and $8800 addressing modes to access BG and Window Tile Data"
+    static constexpr u16 vramAddressingMode[2][2] = { { 0x8000, 0x8800 }, { 0x9000, 0x8800 } };
+}
+
 gb::PPU::PPU(GBConsole* device)
     : system(device), LCDControl({}), LCDStatus({})
 {
@@ -12,15 +18,15 @@ auto gb::PPU::read(u16 address) -> u8
 
     if (address >= 0x8000 && address <= 0x9FFF)
     {
-        if (LCDStatus.ModeFlag == 3)
-            return 0xFF;
+        /*if (LCDStatus.ModeFlag == 3)
+            return 0xFF;*/
 
         dataRead = VRAM[address & 0x1FFF];
     }
     else if (address >= 0xFE00 && address <= 0xFE9F)
     {
-        if (LCDStatus.ModeFlag == 3 || LCDStatus.ModeFlag == 2)
-            return 0xFF;
+        /*if (LCDStatus.ModeFlag == 3 || LCDStatus.ModeFlag == 2)
+            return 0xFF;*/
 
         dataRead = OAM[address & 0x9F];
     }
@@ -56,15 +62,15 @@ auto gb::PPU::write(u16 address, u8 data) -> void
 {
     if (address >= 0x8000 && address <= 0x9FFF)
     {
-        if (LCDStatus.ModeFlag == 3)
-            return;
+        /*if (LCDStatus.ModeFlag == 3)
+            return;*/
 
         VRAM[address & 0x1FFF] = data;
     }
     else if (address >= 0xFE00 && address <= 0xFE9F)
     {
-        if (LCDStatus.ModeFlag == 3 || LCDStatus.ModeFlag == 2)
-            return;
+        /*if (LCDStatus.ModeFlag == 3 || LCDStatus.ModeFlag == 2)
+            return;*/
 
         OAM[address & 0x9F] = data;
     }
@@ -94,10 +100,13 @@ auto gb::PPU::reset() -> void
 
 auto gb::PPU::clock() -> void
 {
+    if (LYC == LY)
+        LCDStatus.LYCLY_Flag = 1;
+
     // STAT Interrupt only triggered in rising edge, that is from low 0 to high 1
     if (!system->getInterruptState(gb::GBConsole::InterruptType::STAT) && LCDControl.LCDenable)
     {
-        if (((LYC == LY) && LCDStatus.LYCLYSTATIntrSrc) || 
+        if ((LCDStatus.LYCLY_Flag && LCDStatus.LYCLYSTATIntrSrc) ||
             (LCDStatus.ModeFlag == 0 && LCDStatus.Mode0STATIntrSrc) ||
             (LCDStatus.ModeFlag == 2 && LCDStatus.Mode2STATIntrSrc) ||
             (LCDStatus.ModeFlag == 1 && LCDStatus.Mode1STATIntrSrc))
